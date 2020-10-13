c50d431265955225f55fd2fb54f231778e4f6006
Maxim Kondratev <gooooglemax@gmail.com>
2020-10-13 19:38:05 +0300

    enter_screen and authorization_screen

--- app/src/main/java/com/example/bookcrossing/MainActivity.java
+++ app/src/main/java/com/example/bookcrossing/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.bookcrossing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.enter_screen);
+    }
+
+    public void EnterButton(View view) {
+        setContentView(R.layout.authorization_screen);
     }
 }
