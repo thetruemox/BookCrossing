23149d6cbc5a6183eca2240c6b1f83965a9b2046
Maxim Kondratev <gooooglemax@gmail.com>
2020-11-15 15:59:15 +0300

    Профиль

--- app/src/main/java/com/example/bookcrossing/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/bookcrossing/ui/gallery/GalleryFragment.java
@@ -22,14 +22,7 @@ public class GalleryFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
         return root;
     }
 }
