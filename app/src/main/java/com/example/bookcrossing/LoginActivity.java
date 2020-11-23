540d802348d83299bd508961a758a2c58b9a316a
Maxim Kondratev <gooooglemax@gmail.com>
2020-11-23 22:20:44 +0300

    Скролл бары и проверки на логины, пароли etc

--- app/src/main/java/com/example/bookcrossing/LoginActivity.java
+++ app/src/main/java/com/example/bookcrossing/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AppCompatActivity
 {
     String login = "admin", email, name, password = "admin";
     EditText obj_aut_login, obj_aut_password;
-    TextView obj_input_error;
+    TextView aut_error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,12 +21,18 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.enter_screen);
     }
 
+    private void show_error(TextView obj, String error_text)
+    {
+        obj.setText(error_text);
+        obj.setVisibility(View.VISIBLE);
+    }
+
     public void EnterButton(View view)
     {
         setContentView(R.layout.authorization_screen);
 
-        obj_input_error = (TextView)findViewById(R.id.invalid_input);
-        obj_input_error.setVisibility(View.INVISIBLE);
+        aut_error = (TextView)findViewById(R.id.invalid_input);
+        aut_error.setVisibility(View.INVISIBLE);
 
         obj_aut_login = (EditText)findViewById(R.id.editTextTextEmailAddress);
         obj_aut_password = (EditText)findViewById(R.id.editTextTextPassword);
@@ -34,29 +40,41 @@ public class LoginActivity extends AppCompatActivity
 
     public void enterMain(View view)
     {
-        if (obj_aut_login.getText().toString().equals(login) && obj_aut_password.getText().toString().equals(password))
+        if (obj_aut_login.getText().toString().isEmpty() || obj_aut_password.getText().toString().isEmpty())
+        {
+            show_error(aut_error, "Не все поля заполнены!");
+            return;
+        }
+
+        if (obj_aut_login.getText().toString().equals(this.login) && obj_aut_password.getText().toString().equals(this.password))
         {
             android.content.Intent intent = new android.content.Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
+            return;
         }
         else
         {
-            Log.d("STATE", obj_aut_login.getText().toString());
-            Log.d("STATE", obj_aut_password.getText().toString());
+            Log.d("STATE", "obj login = " + obj_aut_login.getText().toString());
+            Log.d("STATE", "obj pass = " + obj_aut_password.getText().toString());
 
-            obj_input_error.setVisibility(View.VISIBLE);
+            Log.d("STATE", "var login = " + this.login);
+            Log.d("STATE", "var pass = " + this.password);
+
+            show_error(aut_error, "Неправильный логин или пароль!");
+
+            return;
         }
     }
 
     EditText obj_reg_login, obj_reg_name, obj_reg_email, obj_reg_password, obj_reg_repeat_password;
-    TextView error_passwords_not_equal;
+    TextView reg_error;
 
     public void newAccount(View view)
     {
         setContentView(R.layout.registration_screen);
 
-        error_passwords_not_equal = (TextView)findViewById(R.id.textView3);
-        error_passwords_not_equal.setVisibility(View.INVISIBLE);
+        reg_error = (TextView)findViewById(R.id.textView3);
+        reg_error.setVisibility(View.INVISIBLE);
 
         obj_reg_login = (EditText)findViewById(R.id.editTextTextEmailAddress2);
         obj_reg_name = (EditText)findViewById(R.id.editTextTextPersonName4);
@@ -67,6 +85,12 @@ public class LoginActivity extends AppCompatActivity
 
     public void createAccount(View view)
     {
+        if (obj_reg_password.getText().toString().isEmpty() || obj_reg_email.getText().toString().isEmpty() || obj_reg_name.getText().toString().isEmpty() || obj_reg_login.getText().toString().isEmpty())
+        {
+            show_error(reg_error, "Все поля должны быть заполнены!");
+            return;
+        }
+
         if (obj_reg_password.getText().toString().equals(obj_reg_repeat_password.getText().toString()))
         {
             login = obj_reg_login.getText().toString();
@@ -74,18 +98,20 @@ public class LoginActivity extends AppCompatActivity
             name = obj_reg_name.getText().toString();
             email = obj_reg_email.getText().toString();
 
-            Log.d("STATE", login);
-            Log.d("STATE", password);
-            Log.d("STATE", name);
-            Log.d("STATE", email);
-
             setContentView(R.layout.authorization_screen);
-            obj_input_error.setVisibility(View.INVISIBLE);
+
+            obj_aut_login = (EditText)findViewById(R.id.editTextTextEmailAddress);
+            obj_aut_password = (EditText)findViewById(R.id.editTextTextPassword);
+
+            aut_error = (TextView)findViewById(R.id.invalid_input);
+            aut_error.setVisibility(View.INVISIBLE);
+
+            return;
         }
         else
         {
-
-            error_passwords_not_equal.setVisibility(View.VISIBLE);
+            show_error(reg_error, "Пароли не совпадают!" );
+            return;
         }
 
 
@@ -93,7 +119,15 @@ public class LoginActivity extends AppCompatActivity
 
 
 
-    public void getBack(View view) { setContentView(R.layout.authorization_screen); }
+    public void getBack(View view)
+    {
+        setContentView(R.layout.authorization_screen);
+        obj_aut_login = (EditText)findViewById(R.id.editTextTextEmailAddress);
+        obj_aut_password = (EditText)findViewById(R.id.editTextTextPassword);
+
+        aut_error = (TextView)findViewById(R.id.invalid_input);
+        aut_error.setVisibility(View.INVISIBLE);
+    }
 
 
 
